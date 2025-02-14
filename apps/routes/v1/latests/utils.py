6983e7a55b6ce6cb01bc625b6fa9387cca72bfc6
JulienPeloton <peloton@lal.in2p3.fr>
2025-02-14 14:50:30 +0100

    Better checks

--- apps/routes/v1/latests/utils.py
+++ apps/routes/v1/latests/utils.py
@@ -28,7 +28,15 @@ def extract_feature(string, pos):
     """
     """
     string = string.replace("NaN", "np.nan")
-    return list(eval(string))[pos]
+    mylist = list(eval(string))
+
+    if np.inan(mylist):
+        return np.nan
+    elif isinstance(mylist, list):
+        if len(mylist) > 0:
+            return list(eval(string))[pos]
+        else:
+            return np.nan
 
 @profile
 def extract_object_from_class(payload: dict, return_raw: bool = False) -> pd.DataFrame:
