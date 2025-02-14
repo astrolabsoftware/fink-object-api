f98874d5b4efada53db12f377d1a72d9a81e7fe6
JulienPeloton <peloton@lal.in2p3.fr>
2025-02-14 14:57:30 +0100

    No mag rate

--- apps/routes/v1/latests/utils.py
+++ apps/routes/v1/latests/utils.py
@@ -195,7 +195,7 @@ def extract_object_from_class(payload: dict, return_raw: bool = False) -> pd.Dat
         if "d:lc_features_g" in pdf.columns:
             f1 = pdf["d:lc_features_g"].apply(lambda x: extract_feature(x, 9)) < 0
             f2 = pdf["d:lc_features_r"].apply(lambda x: extract_feature(x, 9)) < 0
-            pdf = pdf[f0 & (f1 | f2)]
+            pdf = pdf[(f1 | f2)]
         else:
             pdf = pdf[f0]
     elif payload.get("trend", None) == "fading":
@@ -203,7 +203,7 @@ def extract_object_from_class(payload: dict, return_raw: bool = False) -> pd.Dat
         if "d:lc_features_g" in pdf.columns:
             f1 = pdf["d:lc_features_g"].apply(lambda x: extract_feature(x, 9)) > 0
             f2 = pdf["d:lc_features_r"].apply(lambda x: extract_feature(x, 9)) > 0
-            pdf = pdf[f0 & (f1 | f2)]
+            pdf = pdf[(f1 | f2)]
         else:
             pdf = pdf[f0]
     elif payload.get("trend", None) == "new_low_state":
