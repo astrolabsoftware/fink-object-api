2a1ceba78c093a8d7519fa9bedc1944366a8ff6e
JulienPeloton <peloton@lal.in2p3.fr>
2025-09-09 11:39:27 +0200

    Push tests and profiling for cutouts

---
+++ apps/routes/v1/lsst/cutouts/profiling.py
@@ -0,0 +1,29 @@
+# Copyright 2025 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Call format_and_send_cutout"""
+
+from apps.routes.v1.ztf.cutouts.utils import format_and_send_cutout
+from flask import Flask
+
+app = Flask("Profile cutouts")
+
+payload = {
+    "diaObjectId": "169298437355340113",
+    "kind": "All",
+    "output-format": "array",
+}
+
+with app.app_context():
+    format_and_send_cutout(payload)
