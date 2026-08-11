a86cf04858a6081337f93fa601cea5a1a07bc5ea
JulienPeloton <peloton@lal.in2p3.fr>
2026-08-11 11:42:51 +0200

    sanitize values before comparison

--- apps/routes/v1/lsst/ssoft/utils.py
+++ apps/routes/v1/lsst/ssoft/utils.py
@@ -30,7 +30,8 @@ from line_profiler import profile
 
 def get_rid_nan_inf(pdf, col):
     """Remove problematic values"""
-    return pdf[~pd.isnull(pdf[col])]
+    is_decimal = pdf[col].apply(lambda x: str(x).isdecimal())
+    return pdf[~pd.isnull(pdf[col]) and is_decimal]
 
 
 @profile
